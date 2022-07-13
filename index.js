73ebc411897b7f354ff0d13d28ab67326f00e6df
Suresh Kumar <99480758+rex-suresh@users.noreply.github.com>
2022-07-13 16:55:32 +0530

    Passing sessions from app level

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const config = require(process.env.SERVER_CONFIG);
 const { startServer } = require(config.server);
 const { createRouter } = require(config.app);
 
-const router = createRouter(config.public);
+const sessions = {};
+const router = createRouter(config.public, sessions);
 startServer(config.port, router);
