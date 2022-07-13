7886ad83b4eea9ce62687c9d16a1227494612a44
Suresh Kumar <99480758+rex-suresh@users.noreply.github.com>
2022-07-13 16:58:22 +0530

    Added statusCode 200 check for xhr.js

--- public/scripts/xhr.js
+++ public/scripts/xhr.js
@@ -1,6 +1,12 @@
 const xhr = (xhrHandler, method, path, bodyParams) => {
   const xhr = new XMLHttpRequest();
-  xhr.onload = (event) => xhrHandler(xhr, event);
+  xhr.onload = (event) => {
+    if (xhr.status === 200) {
+      xhrHandler(xhr, event)
+      return;
+    }
+    console.log(`Unable to fetch ${path} at ${method}`);
+  };
   xhr.open(method, path);
   if (method === 'POST') {
     xhr.send(bodyParams);
